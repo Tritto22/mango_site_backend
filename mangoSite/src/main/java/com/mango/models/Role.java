48f1709781b72940c621548ce706bfd215f882cf
Simone <tritto.simone@gmail.com>
2023-10-10 12:44:10 +0200

    create auth models

---
+++ mangoSite/src/main/java/com/mango/models/Role.java
@@ -0,0 +1,26 @@
+package com.mango.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="TBL_ROLES")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+	
+	public Role() {
+
+	}
+	
+	public Role(ERole name) {
+	    this.name = name;
+	}
+}
