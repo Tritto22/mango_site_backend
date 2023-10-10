b17decfa60c9ff60e29f9bbc8e2e97534b34a578
Simone <tritto.simone@gmail.com>
2023-10-10 11:35:31 +0200

    init

---
+++ mangoSite/src/main/java/com/mango/MangoSiteApplication.java
@@ -0,0 +1,13 @@
+package com.mango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoSiteApplication.class, args);
+	}
+
+}
