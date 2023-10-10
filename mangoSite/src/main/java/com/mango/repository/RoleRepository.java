c245cbe08f03f3fc98cb924f138ddd93eead6a80
Simone <tritto.simone@gmail.com>
2023-10-10 12:49:46 +0200

    create interface repository

---
+++ mangoSite/src/main/java/com/mango/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.mango.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mango.models.ERole;
+import com.mango.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+	Optional<Role> findByName(ERole name);
+}
