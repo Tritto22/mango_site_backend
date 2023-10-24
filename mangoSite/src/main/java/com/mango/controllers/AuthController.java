2f6fe846a2ba719c2d22b535d4f873795127ddf4
Simone <tritto.simone@gmail.com>
2023-10-24 19:52:26 +0200

    AuthController

--- mangoSite/src/main/java/com/mango/controllers/AuthController.java
+++ mangoSite/src/main/java/com/mango/controllers/AuthController.java
@@ -20,9 +20,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mango.models.ERole;
+import com.mango.models.Role;
+import com.mango.models.User;
+import com.mango.payload.request.LoginRequest;
+import com.mango.payload.request.SignupRequest;
+import com.mango.payload.response.JwtResponse;
+import com.mango.payload.response.MessageResponse;
 import com.mango.repository.RoleRepository;
 import com.mango.repository.UserRepository;
 import com.mango.security.jwt.JwtUtils;
+import com.mango.security.services.UserDetailsImpl;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -43,4 +51,81 @@ public class AuthController {
 
 	@Autowired
 	JwtUtils jwtUtils;
+	
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+	        new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+	    );
+
+	    SecurityContextHolder.getContext().setAuthentication(authentication);
+	    String jwt = jwtUtils.generateJwtToken(authentication);
+	    
+	    UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();    
+	    List<String> roles = userDetails.getAuthorities().stream()
+	        .map(item -> item.getAuthority())
+	        .collect(Collectors.toList());
+
+	    return ResponseEntity.ok(new JwtResponse(jwt, 
+	                         userDetails.getId(), 
+	                         userDetails.getUsername(), 
+	                         userDetails.getEmail(), 
+	                         roles));
+	  }
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+	      return ResponseEntity
+	    		  .badRequest()
+	    		  .body(new MessageResponse("Error: Username is already taken!"));
+	    }
+
+	    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+	      return ResponseEntity
+	    		  .badRequest()
+	    		  .body(new MessageResponse("Error: Email is already in use!"));
+	    }
+
+	    // Create new user's account
+	    User user = new User(signUpRequest.getUsername(), 
+	               signUpRequest.getEmail(),
+	               encoder.encode(signUpRequest.getPassword()));
+
+	    Set<String> strRoles = signUpRequest.getRole();
+	    Set<Role> roles = new HashSet<>();
+
+	    if (strRoles == null) {
+	      Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+	    		  			.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+	      roles.add(userRole);
+	    } else {
+	      strRoles.forEach(role -> {
+	        switch (role) {
+	        case "admin":
+	          Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+	              				.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+	          roles.add(adminRole);
+
+	          break;
+	        case "mod":
+	          Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+	              				.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+	          roles.add(modRole);
+
+	          break;
+	        default:
+	          Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+	              				.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+	          roles.add(userRole);
+	        }
+	      });
+	    }
+
+	    user.setRoles(roles);
+	    userRepository.save(user);
+
+	    return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
 }
